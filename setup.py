cb7f5f3afe065fb56032787a0a830abdee429464
Udo Polder <udo@polder.pro>
2016-05-17 21:00:43 +0200

    .

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+#python setup.py sdist
+
 setup(
     name='django-admin-field-search',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
