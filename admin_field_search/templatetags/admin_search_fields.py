1642c3127a9f06d18b9566f9c26acc25a822760b
Udo Polder <udo@polder.pro>
2016-06-20 18:33:39 +0200

    search init hidden option

--- admin_field_search/templatetags/admin_search_fields.py
+++ admin_field_search/templatetags/admin_search_fields.py
@@ -2,17 +2,23 @@
 
 from django.contrib.admin.views.main import SEARCH_VAR, PAGE_VAR
 from django.template import Library
+from django.conf import settings
 
 register = Library()
 
 @register.inclusion_tag('admin/admin_search_fields/admin_search_fields.html')
 def admin_search(cl):
+    hide_search = False
+    if settings.ADMIN_FIELD_SEARCH_HIDE_INITIAL is not None:
+        hide_search = settings.ADMIN_FIELD_SEARCH_HIDE_INITIAL
+
     return {
         'cl':cl,
         'search_var': SEARCH_VAR,
         'query_values': cl.model_admin.query_values,
         'admin_search_fields': cl.model_admin.admin_search_fields,
-        'page_var' : PAGE_VAR,
+        'page_var': PAGE_VAR,
+        'hide_search': hide_search
     }
 
 @register.filter
